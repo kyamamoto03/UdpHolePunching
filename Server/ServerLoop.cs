e436c60ee963255c3a3bf5643dd2597c59df6ad6
Kohji Yamamoto <k.yamamoto03@iforcom.jp>
2020-03-31 15:04:12 +0900

    アドレスと動的に取得するように変更した

--- Server/ServerLoop.cs
+++ Server/ServerLoop.cs
@@ -6,6 +6,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
+using System.Net.NetworkInformation;
 
 namespace Server
 {
@@ -19,9 +20,11 @@ namespace Server
         {
             Console.WriteLine("Start");
 
+            var WaitingServerAddress = IPAddresses[0];
             var udpClient = new UdpClient(PORT_NUMBER);
-            IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, PORT_NUMBER);
+            IPEndPoint groupEP = new IPEndPoint(WaitingServerAddress, PORT_NUMBER);
 
+            Console.WriteLine($"Waiting Address:{WaitingServerAddress}");
             //クライアントからのメッセージ(UDPホールパンチング）を待つ
             //groupEPにNATが変換したアドレス＋ポート番号は入ってくる
             var TargetAddress = Encoding.UTF8.GetString(udpClient.Receive(ref groupEP));
@@ -36,14 +39,57 @@ namespace Server
 
             PunchingSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
             //ソースアドレスを設定する(NATが変換できるように、クライアントが指定した宛先を設定)
-            PunchingSocket.Bind(new IPEndPoint(groupEP.Address, PORT_NUMBER));
-            
+            PunchingSocket.Bind(new IPEndPoint(WaitingServerAddress, PORT_NUMBER));
+
             PunchingPoint = new IPEndPoint(IPAddress.Parse(ip), port);
 
-            await MainLoop();
+            await MainLoop(cancellationToken);
         }
 
-        private async Task MainLoop()
+        /// <summary>
+        /// IPアドレスを取得(IPV4のみ)
+        /// </summary>
+        internal List<IPAddress> IPAddresses
+        {
+            get
+            {
+                List<IPAddress> rets = new List<IPAddress>();
+                // 物理インターフェース情報をすべて取得
+                var interfaces = NetworkInterface.GetAllNetworkInterfaces();
+
+                // 各インターフェースごとの情報を調べる
+                foreach (var adapter in interfaces)
+                {
+                    // 有効なインターフェースのみを対象とする
+                    if (adapter.OperationalStatus != OperationalStatus.Up)
+                    {
+                        continue;
+                    }
+
+                    // インターフェースに設定されたIPアドレス情報を取得
+                    var properties = adapter.GetIPProperties();
+
+                    // 設定されているすべてのユニキャストアドレスについて
+                    foreach (var unicast in properties.UnicastAddresses)
+                    {
+                        if (unicast.Address.AddressFamily == AddressFamily.InterNetwork)
+                        {
+                            // IPv4アドレス
+                            rets.Add(unicast.Address);
+                        }
+                    }
+                }
+
+                return rets;
+            }
+        }
+        
+        /// <summary>
+        /// メインループ
+        /// 1秒ごとに時間を送る
+        /// </summary>
+        /// <returns></returns>
+        private async Task MainLoop(CancellationToken stoppingToken)
         {
             var Buffer = new byte[128];
 
@@ -56,7 +102,7 @@ namespace Server
                 //サーバからクライアントへ送信
                 PunchingSocket.SendTo(data, SocketFlags.None, PunchingPoint);
 
-                await Task.Delay(1000);
+                await Task.Delay(1000, stoppingToken);
             }
         }
 
